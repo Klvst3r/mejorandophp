8d10699df77afca0052aafc8f8d2484c11fcfdff
Klvst3r <klvst3r@gmail.com>
2018-05-11 20:56:48 -0500

    Mixing PHP and HTML

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mejorando Laravel</title>
+</head>
+<body>
+	<h1>Mejorando con Laravel</h1>
+	<?php
+
+	echo "PHP & HTML5";
+
+	?>
+</body>
+</html>
