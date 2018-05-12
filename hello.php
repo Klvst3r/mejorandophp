de7237e9326b703ba2454f25514b466522fa7cec
Klvst3r <klvst3r@gmail.com>
2018-05-11 21:13:12 -0500

    First View

--- hello.php
+++ hello.php
@@ -2,19 +2,4 @@
 
 $languaje = "PHP";
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Mejorando Laravel</title>
-</head>
-<body>
-	<h1>Mejorando con Laravel</h1>
-	<?php
-
-	echo "<strong>" . $languaje . "</strong> & HTML5";
-
-	?>
-</body>
-</html>
+require "view.php";
---
+++ view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mejorando Laravel</title>
+</head>
+<body>
+	<h1>Mejorando con Laravel</h1>
+	<?php
+
+	echo "<strong>" . $languaje . "</strong> & HTML5";
+
+	?>
+</body>
+</html>
