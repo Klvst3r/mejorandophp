e2f08664f45642fc3d938337b1d1e922a322c4a4
Klvst3r <klvst3r@gmail.com>
2018-05-16 21:53:07 -0500

    Encapsulate code in functions

--- hello.php
+++ hello.php
@@ -4,4 +4,10 @@ $confidencial = "wow such private very confidencial";
 
 $languaje = "PHP";
 
-require "view.php";
+//Declarando a una funcion
+function view($languaje){
+	require "view.php";
+}
+
+//Llamando a una funcion
+view($languaje);
