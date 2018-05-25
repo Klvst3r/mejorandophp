d6bfe953e34816b1d31efe84ada007a120927f43
Klvst3r <klvst3r@gmail.com>
2018-05-25 00:05:40 -0500

    Refactorizing Frontend Controller

--- helpers.php
+++ helpers.php
@@ -4,3 +4,18 @@ function view($template, $vars = array() ){
 	extract($vars);
 	require "views/$template.tpl.php";
 } 
+
+function controller($name){
+	
+	$file = "controllers/$name.php";
+	//exit($file);
+	if(file_exists($file)){
+		
+		require $file;
+	}else{
+		header("HTTP/1.0 404 Not Found");
+		exit("Página no encontrada");
+		
+	}
+
+}
--- index.php
+++ index.php
@@ -6,30 +6,9 @@
 require 'config.php';
 require 'helpers.php';
 
-// Llamar a los controladores individuales
-
-
-//index.php //Llamamos controlador home
+// Llamar al controlador indicado
 
 if(empty($_GET['url'])){
-	require 'controllers/home.php';
+	$_GET['url'] = 'home';
 }
-//index.php?url=contactos
-elseif($_GET['url'] == 'contactos'){
-	require 'controllers/contactos.php';
-}
-
-//print_r()
-//var_dump() //utilizar para ver que esta sucediendo
-
-
-//Imprimimos salida
-//var_dump($_GET);
-
-
-//index.php?url=asdasd
-else{
-	//error 404
-	header("HTTP/1.0 404 Not Found");
-	exit("Página no encontrada");
-}	
+controller($_GET['url']);
